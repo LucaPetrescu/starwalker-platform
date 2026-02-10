33ea075b62cbd542652c17879dd99f884ac7b459
Luca Petrescu <luca.petrescu@inthepocket.com>
2026-02-11 00:04:34 +0200

    feat: add new files

---
+++ src/main/java/com/space/starwalkerplatform/StarwalkerplatformApplication.java
@@ -0,0 +1,13 @@
+package com.space.starwalkerplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarwalkerplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarwalkerplatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/space/starwalkerplatform/StarwalkerplatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.space.starwalkerplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarwalkerplatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
